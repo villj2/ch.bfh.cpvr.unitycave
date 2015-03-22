0b6237fab2d4b87270be54829e3155ffe059d3fe
villj2 <villj2@bfh.ch>
2015-03-22 21:38:38 +0100

    Networking Test Complete

--- testlab/NetworkingTest/Assets/Scripts/Player.cs
+++ testlab/NetworkingTest/Assets/Scripts/Player.cs
@@ -4,13 +4,44 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	public float speed = 10f;
+
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
 	
 	void Update()
 	{
 		if (networkView.isMine)
 		{
 			InputMovement();
+			InputColorChange();
 		}
+		else
+		{
+			SyncedMovement();
+		}
+	}
+
+	private void InputColorChange()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+	}
+	
+	[RPC] void ChangeColorTo(Vector3 color)
+	{
+		renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+		
+		if (networkView.isMine)
+			networkView.RPC("ChangeColorTo", RPCMode.OthersBuffered, color);
+	}
+
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
 	}
 	
 	void InputMovement()
@@ -26,5 +57,31 @@ public class Player : MonoBehaviour
 		
 		if (Input.GetKey(KeyCode.A))
 			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	{
+		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
+		if (stream.isWriting)
+		{
+			syncPosition = rigidbody.position;
+			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
+		}
+		else
+		{
+			stream.Serialize(ref syncPosition);
+			stream.Serialize(ref syncVelocity);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rigidbody.position;
+		}
 	}
 }
