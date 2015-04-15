c2ca64b49b8bca81e93169676a79b6baa88cd8f6
villj2 <villj2@bfh.ch>
2015-04-15 17:09:52 +0200

    AngryBots MiddleVR Update

---
+++ testlab/MiddleVR AngryBots Unity4.6/Assets/Scripts/Custom/Custom.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Custom : MonoBehaviour {
+
+	void Start() {
+		print("Starting " + Time.time);
+		StartCoroutine(WaitAndPrint(2.0F));
+		print("Before WaitAndPrint Finishes " + Time.time);
+	}
+	IEnumerator WaitAndPrint(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		print("WaitAndPrint " + Time.time);
+	}
+
+	/*// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Start Coroutine");
+		StartCoroutine ("AttachScripts");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerable AttachScripts()
+	{
+		Debug.Log ("Coroutine started");
+
+		yield return new WaitForSeconds(5);
+
+		Debug.Log ("Coroutine waited");
+
+		Camera.main.gameObject.AddComponent ("VRApplySharedTransform");
+		Camera.main.gameObject.AddComponent ("VRClusterObject");
+		Camera.main.gameObject.AddComponent ("VRShareTransform");
+	}*/
+}
