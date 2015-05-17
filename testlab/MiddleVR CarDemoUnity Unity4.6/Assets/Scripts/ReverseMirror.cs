6030a5640fb9d3b6ceee1dd8039ecc64a3bf6bc4
inved1 <inved1@bfh.ch>
2015-05-17 20:33:50 +0200

    some camera tests

---
+++ testlab/MiddleVR CarDemoUnity Unity4.6/Assets/Scripts/ReverseMirror.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReverseMirror : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rect  v = camera.rect;
+
+		v.x = 0.8f;
+		v.y = 0.8f;
+		camera.rect = v;
+		Debug.Log (v.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
